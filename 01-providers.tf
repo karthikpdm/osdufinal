df94182c766bae039e9c6a4a28b8343e82e76722
karthikpdm <karthikbm.pdm19@rvce.edu.in>
2025-07-22 21:48:56 +0530

    changed

--- 01-providers.tf
+++ 01-providers.tf
@@ -120,6 +120,18 @@ provider "kubernetes" {
 }
 
 
+provider "helm" {
+  kubernetes = {  # ← Add this = sign on line 124
+    host                   = aws_eks_cluster.osdu_eks_cluster_regional.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.osdu_eks_cluster_regional.certificate_authority[0].data)
+    
+    exec {  # ← Keep this without = sign (line 128 is correct)
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.osdu_eks_cluster_regional.name, "--region", "us-east-1"]
+    }
+  }
+}
 
 
 
