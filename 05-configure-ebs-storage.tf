716d9c1b048b58b02000faf26d95826273411108
karthikpdm <karthikbm.pdm19@rvce.edu.in>
2025-07-22 16:40:59 +0530

    changed

--- 05-configure-ebs-storage.tf
+++ 05-configure-ebs-storage.tf
@@ -1,8 +1,98 @@
-# Configuring the EBS CSI Driver for nodes to write to EBS volume
+# # Configuring the EBS CSI Driver for nodes to write to EBS volume
+# # data "tls_certificate" "osdu_certificate" {
+# #   url        = aws_eks_cluster.osdu_eks_cluster.identity[0].oidc[0].issuer
+# #   depends_on = [aws_eks_cluster.osdu_eks_cluster_regional]
+# # }
+
 # data "tls_certificate" "osdu_certificate" {
-#   url        = aws_eks_cluster.osdu_eks_cluster.identity[0].oidc[0].issuer
-#   depends_on = [aws_eks_cluster.osdu_eks_cluster_regional]
+#   url = aws_eks_cluster.osdu_eks_cluster_regional.identity[0].oidc[0].issuer
 # }
+
+# # Configuring the open-id provider for EBS CSI Driver
+# resource "aws_iam_openid_connect_provider" "osdu_openid_provider" {
+#   client_id_list  = ["sts.amazonaws.com"]
+#   thumbprint_list = [data.tls_certificate.osdu_certificate.certificates[0].sha1_fingerprint]
+#   url             = aws_eks_cluster.osdu_eks_cluster.identity[0].oidc[0].issuer
+#   depends_on      = [aws_eks_cluster.osdu_eks_cluster]
+# }
+
+# # Roles for the EBS CSI Driver
+# data "aws_iam_policy_document" "osdu_csi_policy" {
+#   statement {
+#     actions = ["sts:AssumeRoleWithWebIdentity"]
+#     effect  = "Allow"
+
+#     condition {
+#       test     = "StringEquals"
+#       variable = "${replace(aws_iam_openid_connect_provider.osdu_openid_provider.url, "https://", "")}:sub"
+#       values   = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+#     }
+
+#     principals {
+#       identifiers = [aws_iam_openid_connect_provider.osdu_openid_provider.arn]
+#       type        = "Federated"
+#     }
+#   }
+#   depends_on = [aws_iam_openid_connect_provider.osdu_openid_provider]
+# }
+
+# # Apply the policy to the role
+# resource "aws_iam_role" "osdu_ebs_csi_driver_role" {
+#   assume_role_policy = data.aws_iam_policy_document.osdu_csi_policy.json
+#   name               = "osdu_ebs_csi_driver_role"
+# }
+
+# resource "aws_iam_role_policy_attachment" "osdu_ebs_csi_driver_policy" {
+#   role       = aws_iam_role.osdu_ebs_csi_driver_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+# }
+
+# # Configuring the EBS CSI Driver
+# resource "aws_eks_addon" "osdu_csi_addon" {
+#   cluster_name             = var.eks_cluster_name
+#   addon_name               = var.csi_ebs_driver_name
+#   addon_version            = var.csi_ebs_driver_version
+#   service_account_role_arn = aws_iam_role.osdu_ebs_csi_driver_role.arn
+
+#   depends_on = [
+#     aws_instance.osdu_istio_node,
+#     aws_instance.osdu_backend_node,
+#     aws_instance.osdu_frontend_node
+#   ]
+# }
+
+# # Configure storage class for osdu
+# resource "kubernetes_storage_class" "osdu_ebs_storage" {
+#   metadata {
+#     name = "osdu-ebs-storage"
+#     annotations = {
+#       "storageclass.kubernetes.io/is-default-class" = "true"
+#     }
+#   }
+
+#   storage_provisioner = "ebs.csi.aws.com"
+#   parameters = {
+#     type   = "gp2"
+#     fsType = "ext4"
+#   }
+
+#   reclaim_policy         = "Delete"
+#   volume_binding_mode    = "WaitForFirstConsumer"
+#   allow_volume_expansion = "true"
+
+#   depends_on = [aws_eks_addon.osdu_csi_addon]
+# }
+
+
+
+
+
+
+
+
+
+
+
 
 data "tls_certificate" "osdu_certificate" {
   url = aws_eks_cluster.osdu_eks_cluster_regional.identity[0].oidc[0].issuer
@@ -12,8 +102,8 @@ data "tls_certificate" "osdu_certificate" {
 resource "aws_iam_openid_connect_provider" "osdu_openid_provider" {
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = [data.tls_certificate.osdu_certificate.certificates[0].sha1_fingerprint]
-  url             = aws_eks_cluster.osdu_eks_cluster.identity[0].oidc[0].issuer
-  depends_on      = [aws_eks_cluster.osdu_eks_cluster]
+  url             = aws_eks_cluster.osdu_eks_cluster_regional.identity[0].oidc[0].issuer
+  depends_on      = [aws_eks_cluster.osdu_eks_cluster_regional]
 }
 
 # Roles for the EBS CSI Driver
@@ -55,9 +145,9 @@ resource "aws_eks_addon" "osdu_csi_addon" {
   service_account_role_arn = aws_iam_role.osdu_ebs_csi_driver_role.arn
 
   depends_on = [
-    aws_instance.osdu_istio_node,
-    aws_instance.osdu_backend_node,
-    aws_instance.osdu_frontend_node
+    aws_instance.osdu_istio_node_regional_az1,
+    aws_instance.osdu_backend_node_regional_az2,
+    aws_instance.osdu_frontend_node_regional_az1
   ]
 }
 
