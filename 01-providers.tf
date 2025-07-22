9d1ecff5658319bbf550dd5b7a45ed094f137584
karthikpdm <karthikbm.pdm19@rvce.edu.in>
2025-07-22 21:38:12 +0530

    changed

--- 01-providers.tf
+++ 01-providers.tf
@@ -94,16 +94,18 @@ data "aws_eks_cluster_auth" "bsp_eks" {
 # }
 
 # Helm provider
-provider "kubernetes" {
-  host                   = aws_eks_cluster.osdu_eks_cluster_regional.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.osdu_eks_cluster_regional.certificate_authority[0].data)
-  
-  exec = {  # Make sure this also has = sign
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.osdu_eks_cluster_regional.name, "--region", "us-east-1"]
-  }
-}
+# provider "helm" {
+#   kubernetes = {
+#     host                   = aws_eks_cluster.osdu_eks_cluster_regional.endpoint
+#     cluster_ca_certificate = base64decode(aws_eks_cluster.osdu_eks_cluster_regional.certificate_authority[0].data)
+    
+#     exec = {  # Added = sign here
+#       api_version = "client.authentication.k8s.io/v1beta1"
+#       command     = "aws"
+#       args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.osdu_eks_cluster_regional.name, "--region", "us-east-1"]
+#     }
+#   }
+# }
 
 # Kubernetes provider
 provider "kubernetes" {
