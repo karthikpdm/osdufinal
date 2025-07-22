ecac99d50edb7398acb74aeab7374620d1fc33e3
karthikpdm <karthikbm.pdm19@rvce.edu.in>
2025-07-22 21:40:57 +0530

    changed

--- 06-certificates.tf
+++ 06-certificates.tf
@@ -1,226 +1,226 @@
-# # This script generates the required certificates for https communication
+# This script generates the required certificates for https communication
 
-# # Used for https connection
-# resource "tls_private_key" "osdu_tls_key" {
-#   algorithm = "RSA"
-#   rsa_bits  = 2048
-# }
+# Used for https connection
+resource "tls_private_key" "osdu_tls_key" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
 
-# # Creating a self signed certificate
-# resource "tls_self_signed_cert" "osdu_tls_cert" {
+# Creating a self signed certificate
+resource "tls_self_signed_cert" "osdu_tls_cert" {
 
-#   private_key_pem = tls_private_key.osdu_tls_key.private_key_pem
+  private_key_pem = tls_private_key.osdu_tls_key.private_key_pem
 
-#   subject {
-#     common_name  = "osdu.${local.istio_gateway_domain}"
-#     organization = "OSDU"
-#   }
+  subject {
+    common_name  = "osdu.${local.istio_gateway_domain}"
+    organization = "OSDU"
+  }
 
-#   validity_period_hours = 8760 # 1 year
-#   early_renewal_hours   = 720
-#   is_ca_certificate     = false
+  validity_period_hours = 8760 # 1 year
+  early_renewal_hours   = 720
+  is_ca_certificate     = false
 
-#   allowed_uses = [
-#     "key_encipherment",
-#     "digital_signature",
-#     "server_auth",
-#   ]
+  allowed_uses = [
+    "key_encipherment",
+    "digital_signature",
+    "server_auth",
+  ]
 
-#   dns_names = ["osdu.${local.istio_gateway_domain}"]
-# }
+  dns_names = ["osdu.${local.istio_gateway_domain}"]
+}
 
-# # Creating the TLS secret 
-# resource "kubernetes_secret" "osdu_tls_secret" {
-#   metadata {
-#     name      = "osdu-ingress-tls"
-#     namespace = "istio-system"
-#   }
+# Creating the TLS secret 
+resource "kubernetes_secret" "osdu_tls_secret" {
+  metadata {
+    name      = "osdu-ingress-tls"
+    namespace = "istio-system"
+  }
 
-#   data = {
-#     "tls.crt" = tls_self_signed_cert.osdu_tls_cert.cert_pem
-#     "tls.key" = tls_private_key.osdu_tls_key.private_key_pem
-#   }
+  data = {
+    "tls.crt" = tls_self_signed_cert.osdu_tls_cert.cert_pem
+    "tls.key" = tls_private_key.osdu_tls_key.private_key_pem
+  }
 
-#   type = "kubernetes.io/tls"
+  type = "kubernetes.io/tls"
 
-#   depends_on = [null_resource.update_kubeconfig]
-# }
+  depends_on = [null_resource.update_kubeconfig]
+}
 
-# # Creating for minio
-# resource "tls_private_key" "minio_tls_key" {
-#   algorithm = "RSA"
-#   rsa_bits  = 2048
-# }
+# Creating for minio
+resource "tls_private_key" "minio_tls_key" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
 
-# resource "tls_self_signed_cert" "minio_tls_cert" {
+resource "tls_self_signed_cert" "minio_tls_cert" {
 
-#   private_key_pem = tls_private_key.minio_tls_key.private_key_pem
+  private_key_pem = tls_private_key.minio_tls_key.private_key_pem
 
-#   subject {
-#     common_name  = "minio.${local.istio_gateway_domain}"
-#     organization = "OSDU"
-#   }
+  subject {
+    common_name  = "minio.${local.istio_gateway_domain}"
+    organization = "OSDU"
+  }
 
-#   validity_period_hours = 8760 # 1 year
-#   early_renewal_hours   = 720
-#   is_ca_certificate     = false
+  validity_period_hours = 8760 # 1 year
+  early_renewal_hours   = 720
+  is_ca_certificate     = false
 
-#   allowed_uses = [
-#     "key_encipherment",
-#     "digital_signature",
-#     "server_auth",
-#   ]
+  allowed_uses = [
+    "key_encipherment",
+    "digital_signature",
+    "server_auth",
+  ]
 
-#   dns_names = ["minio.${local.istio_gateway_domain}"]
-# }
+  dns_names = ["minio.${local.istio_gateway_domain}"]
+}
 
-# resource "kubernetes_secret" "minio_tls_secret" {
-#   metadata {
-#     name      = "minio-ingress-tls"
-#     namespace = "istio-system"
-#   }
+resource "kubernetes_secret" "minio_tls_secret" {
+  metadata {
+    name      = "minio-ingress-tls"
+    namespace = "istio-system"
+  }
 
-#   data = {
-#     "tls.crt" = tls_self_signed_cert.minio_tls_cert.cert_pem
-#     "tls.key" = tls_private_key.minio_tls_key.private_key_pem
-#   }
+  data = {
+    "tls.crt" = tls_self_signed_cert.minio_tls_cert.cert_pem
+    "tls.key" = tls_private_key.minio_tls_key.private_key_pem
+  }
 
-#   type = "kubernetes.io/tls"
+  type = "kubernetes.io/tls"
 
-#   depends_on = [null_resource.update_kubeconfig]
-# }
-
-
-# # Creating for S3
-# resource "tls_private_key" "s3_tls_key" {
-#   algorithm = "RSA"
-#   rsa_bits  = 2048
-# }
-
-# resource "tls_self_signed_cert" "s3_tls_cert" {
-
-#   private_key_pem = tls_private_key.s3_tls_key.private_key_pem
-
-#   subject {
-#     common_name  = "s3.${local.istio_gateway_domain}"
-#     organization = "OSDU"
-#   }
+  depends_on = [null_resource.update_kubeconfig]
+}
 
 
-#   validity_period_hours = 8760
-#   early_renewal_hours   = 720
-#   is_ca_certificate     = false
+# Creating for S3
+resource "tls_private_key" "s3_tls_key" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
 
-#   allowed_uses = [
-#     "key_encipherment",
-#     "digital_signature",
-#     "server_auth",
-#   ]
-#   dns_names = ["s3.${local.istio_gateway_domain}"]
-# }
+resource "tls_self_signed_cert" "s3_tls_cert" {
 
-# resource "kubernetes_secret" "s3_tls_secret" {
-#   metadata {
-#     name      = "s3-ingress-tls"
-#     namespace = "istio-system"
-#   }
+  private_key_pem = tls_private_key.s3_tls_key.private_key_pem
 
-#   data = {
-#     "tls.crt" = tls_self_signed_cert.s3_tls_cert.cert_pem
-#     "tls.key" = tls_private_key.s3_tls_key.private_key_pem
-#   }
-
-#   type = "kubernetes.io/tls"
-
-#   depends_on = [null_resource.update_kubeconfig]
-# }
+  subject {
+    common_name  = "s3.${local.istio_gateway_domain}"
+    organization = "OSDU"
+  }
 
 
-# # Creating for keycloak
-# resource "tls_private_key" "keycloak_tls_key" {
-#   algorithm = "RSA"
-#   rsa_bits  = 2048
-# }
+  validity_period_hours = 8760
+  early_renewal_hours   = 720
+  is_ca_certificate     = false
 
-# resource "tls_self_signed_cert" "keycloak_tls_cert" {
+  allowed_uses = [
+    "key_encipherment",
+    "digital_signature",
+    "server_auth",
+  ]
+  dns_names = ["s3.${local.istio_gateway_domain}"]
+}
 
-#   private_key_pem = tls_private_key.keycloak_tls_key.private_key_pem
+resource "kubernetes_secret" "s3_tls_secret" {
+  metadata {
+    name      = "s3-ingress-tls"
+    namespace = "istio-system"
+  }
 
-#   subject {
-#     common_name  = "keycloak.${local.istio_gateway_domain}"
-#     organization = "OSDU"
-#   }
+  data = {
+    "tls.crt" = tls_self_signed_cert.s3_tls_cert.cert_pem
+    "tls.key" = tls_private_key.s3_tls_key.private_key_pem
+  }
 
+  type = "kubernetes.io/tls"
 
-#   validity_period_hours = 8760
-#   early_renewal_hours   = 720
-#   is_ca_certificate     = false
-
-#   allowed_uses = [
-#     "key_encipherment",
-#     "digital_signature",
-#     "server_auth",
-#   ]
-#   dns_names = ["keycloak.${local.istio_gateway_domain}"]
-# }
-
-
-# resource "kubernetes_secret" "keycloak_tls_secret" {
-#   metadata {
-#     name      = "keycloak-ingress-tls"
-#     namespace = "istio-system"
-#   }
-
-#   data = {
-#     "tls.crt" = tls_self_signed_cert.keycloak_tls_cert.cert_pem
-#     "tls.key" = tls_private_key.keycloak_tls_key.private_key_pem
-#   }
-
-#   type = "kubernetes.io/tls"
-
-#   depends_on = [null_resource.update_kubeconfig]
-# }
+  depends_on = [null_resource.update_kubeconfig]
+}
 
 
-# # Creating for airflow
-# resource "tls_private_key" "airflow_tls_key" {
-#   algorithm = "RSA"
-#   rsa_bits  = 2048
-# }
+# Creating for keycloak
+resource "tls_private_key" "keycloak_tls_key" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
 
-# resource "tls_self_signed_cert" "airflow_tls_cert" {
-#   private_key_pem = tls_private_key.airflow_tls_key.private_key_pem
+resource "tls_self_signed_cert" "keycloak_tls_cert" {
 
-#   subject {
-#     common_name  = "airflow.${local.istio_gateway_domain}"
-#     organization = "OSDU"
-#   }
+  private_key_pem = tls_private_key.keycloak_tls_key.private_key_pem
+
+  subject {
+    common_name  = "keycloak.${local.istio_gateway_domain}"
+    organization = "OSDU"
+  }
 
 
-#   validity_period_hours = 8760
-#   early_renewal_hours   = 720
-#   is_ca_certificate     = false
+  validity_period_hours = 8760
+  early_renewal_hours   = 720
+  is_ca_certificate     = false
 
-#   allowed_uses = [
-#     "key_encipherment",
-#     "digital_signature",
-#     "server_auth",
-#   ]
-#   dns_names = ["airflow.${local.istio_gateway_domain}"]
-# }
+  allowed_uses = [
+    "key_encipherment",
+    "digital_signature",
+    "server_auth",
+  ]
+  dns_names = ["keycloak.${local.istio_gateway_domain}"]
+}
 
-# resource "kubernetes_secret" "airflow_tls_secret" {
-#   metadata {
-#     name      = "airflow-ingress-tls"
-#     namespace = "istio-system"
-#   }
 
-#   data = {
-#     "tls.crt" = tls_self_signed_cert.airflow_tls_cert.cert_pem
-#     "tls.key" = tls_private_key.airflow_tls_key.private_key_pem
-#   }
+resource "kubernetes_secret" "keycloak_tls_secret" {
+  metadata {
+    name      = "keycloak-ingress-tls"
+    namespace = "istio-system"
+  }
 
-#   type = "kubernetes.io/tls"
+  data = {
+    "tls.crt" = tls_self_signed_cert.keycloak_tls_cert.cert_pem
+    "tls.key" = tls_private_key.keycloak_tls_key.private_key_pem
+  }
 
-#   depends_on = [null_resource.update_kubeconfig]
-# }
+  type = "kubernetes.io/tls"
+
+  depends_on = [null_resource.update_kubeconfig]
+}
+
+
+# Creating for airflow
+resource "tls_private_key" "airflow_tls_key" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+resource "tls_self_signed_cert" "airflow_tls_cert" {
+  private_key_pem = tls_private_key.airflow_tls_key.private_key_pem
+
+  subject {
+    common_name  = "airflow.${local.istio_gateway_domain}"
+    organization = "OSDU"
+  }
+
+
+  validity_period_hours = 8760
+  early_renewal_hours   = 720
+  is_ca_certificate     = false
+
+  allowed_uses = [
+    "key_encipherment",
+    "digital_signature",
+    "server_auth",
+  ]
+  dns_names = ["airflow.${local.istio_gateway_domain}"]
+}
+
+resource "kubernetes_secret" "airflow_tls_secret" {
+  metadata {
+    name      = "airflow-ingress-tls"
+    namespace = "istio-system"
+  }
+
+  data = {
+    "tls.crt" = tls_self_signed_cert.airflow_tls_cert.cert_pem
+    "tls.key" = tls_private_key.airflow_tls_key.private_key_pem
+  }
+
+  type = "kubernetes.io/tls"
+
+  depends_on = [null_resource.update_kubeconfig]
+}
